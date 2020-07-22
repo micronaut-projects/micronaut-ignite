446a1225f2925c7dbe9e1bae37f639c89ee6f8fd
Michael <polli104@mail.chapman.edu>
2020-07-21 20:00:44 -0700

    implement gridfy advice

--- core-ignite/src/main/java/io/micronaut/ignite/intercept/GridfyAdvice.java
+++ core-ignite/src/main/java/io/micronaut/ignite/intercept/GridfyAdvice.java
@@ -2,36 +2,33 @@ package io.micronaut.ignite.intercept;
 
 import io.micronaut.aop.MethodInterceptor;
 import io.micronaut.aop.MethodInvocationContext;
+import io.micronaut.context.BeanContext;
 import io.micronaut.core.annotation.AnnotationValue;
-import io.micronaut.core.type.Argument;
-import io.micronaut.core.type.ReturnType;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCheckedException;
 import org.apache.ignite.compute.ComputeTask;
 import org.apache.ignite.compute.gridify.Gridify;
 import org.apache.ignite.compute.gridify.GridifyArgument;
 import org.apache.ignite.compute.gridify.GridifyInterceptor;
 import org.apache.ignite.compute.gridify.aop.GridifyArgumentAdapter;
 import org.apache.ignite.compute.gridify.aop.GridifyDefaultTask;
-import org.apache.ignite.internal.IgniteEx;
-import org.apache.ignite.internal.IgnitionEx;
 import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.internal.util.typedef.G;
-import org.apache.ignite.resources.TaskSessionResource;
 
 import javax.inject.Singleton;
-import java.beans.beancontext.BeanContext;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Optional;
 
 import static org.apache.ignite.IgniteState.STARTED;
 
 @Singleton
-public class GridfyInterceptor implements MethodInterceptor<Object,Object> {
+public class GridfyAdvice implements MethodInterceptor<Object,Object> {
 
     private final BeanContext beanContext;
 
 
-    public GridfyInterceptor(BeanContext beanContext) {
+    public GridfyAdvice(BeanContext beanContext) {
         this.beanContext = beanContext;
     }
 
@@ -49,11 +46,11 @@ public class GridfyInterceptor implements MethodInterceptor<Object,Object> {
         String igniteInstanceName = opt.get().stringValue("igniteInstanceName").orElse(opt.get().stringValue("gridName").orElse(null));
 
         if (F.isEmpty(igniteInstanceName)) {
-//            throw new IgniteCheckedException("Grid is not locally started: " + igniteInstanceName);
+            throw new RuntimeException(new IgniteCheckedException("Grid is not locally started: " + igniteInstanceName));
         }
 
         if (G.state(igniteInstanceName) != STARTED) {
-//            throw new IgniteCheckedException("Grid is not locally started: " + igniteInstanceName);
+            throw new RuntimeException(new IgniteCheckedException("Grid is not locally started: " + igniteInstanceName));
         }
 
         // Initialize defaults.
@@ -62,10 +59,17 @@ public class GridfyInterceptor implements MethodInterceptor<Object,Object> {
         GridifyArgument arg = new GridifyArgumentAdapter(mtd.getDeclaringClass(), mtd.getName(),
             mtd.getParameterTypes(), context.getParameterValues(), context.getTarget());
 
-        Class<? extends GridifyInterceptor> value = (Class<? extends GridifyInterceptor>) opt.get().classValue("interceptor").orElse(GridfyInterceptor.class);
+        Class<? extends GridifyInterceptor> value = (Class<? extends GridifyInterceptor>) opt.get().classValue("interceptor").orElse(GridifyInterceptor.class);
 
-        if (!value.equals(GridfyInterceptor.class)) {
-//            if(!value.newInstance().isGridify(opt.))
+        if (!value.equals(GridifyInterceptor.class)) {
+            GridifyInterceptor interceptor = beanContext.getBean(value);
+            Annotation annotation = mtd.getAnnotation(Gridify.class);
+            try {
+                if (!interceptor.isGridify(annotation, arg))
+                    return context.proceed();
+            } catch (IgniteCheckedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         Ignite ignite = G.ignite(igniteInstanceName);
@@ -75,14 +79,13 @@ public class GridfyInterceptor implements MethodInterceptor<Object,Object> {
 
         Class<? extends ComputeTask<GridifyArgument, ?>> taskClass = (Class<? extends ComputeTask<GridifyArgument, ?>>) opt.get().classValue("taskClass").orElse(GridifyDefaultTask.class);
 
-        if(!taskClass.equals(GridifyDefaultTask.class)) {
-            return ignite.compute().withTimeout(timeout).execute(  (Class<? extends ComputeTask<GridifyArgument, Object>>) taskClass, arg);
+        if (!taskClass.equals(GridifyDefaultTask.class)) {
+            return ignite.compute().withTimeout(timeout).execute((Class<? extends ComputeTask<GridifyArgument, Object>>) taskClass, arg);
         }
 
-        if(!taskName.isPresent()){
+        if (!taskName.isPresent()) {
             return ignite.compute().withTimeout(timeout).execute(new GridifyDefaultTask(mtd.getDeclaringClass()), arg);
         }
         return ignite.compute().withTimeout(timeout).execute(taskName.get(), arg);
-
     }
 }
